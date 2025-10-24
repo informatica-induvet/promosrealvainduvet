33121624718867d67460a192d18b0387104c3179
informatica-induvet <informatica@induvet.com>
2025-10-24 14:52:00 -0600

    Update formulario-drive.js

--- js/formulario-drive.js
+++ js/formulario-drive.js
@@ -23,7 +23,7 @@ form.addEventListener("submit", async (e) => {
     const archivoBase64 = reader.result;
 
     try {
-      const response = await fetch("https://script.google.com/macros/s/AKfycbzgAGEqExE58xls3phIDdAr77ZwJBc93NsInKFDNUOizRcLypr3uH2Z3UvWCA5FloSO/exec", {  // Pega acá la URL del Apps Script
+      const response = await fetch("https://script.google.com/macros/s/AKfycbwRxztbaNiTY9gj40IDeIu4M8pS8CJ60aeUEkZHf2iCesaROgbuCm6iEXtEorX_u5F9/exec", {  // Pega acá la URL del Apps Script
         method: "POST",
         body: JSON.stringify({
           name,
